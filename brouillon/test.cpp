1403d5b4e58bc5d758881a9ac457f6264073027e
Theo Portmann <pro.portmanntheo68@gmail.com>
2024-02-20 15:00:09 +0100

    git commit "version1"

---
+++ brouillon/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Theo Portmann on 20/02/2024.
+//
+
+#include "test.h"
---
+++ brouillon/test.h
@@ -0,0 +1,14 @@
+//
+// Created by Theo Portmann on 20/02/2024.
+//
+
+#ifndef TRINATRONICS_TEST_H
+#define TRINATRONICS_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //TRINATRONICS_TEST_H
